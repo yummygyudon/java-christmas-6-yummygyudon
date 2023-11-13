5fb7a850d8db12166e4bc54944fae57f9fdc436c
yummygyudon <bang2brew@gmail.com>
2023-11-13 19:54:54 +0900

    FEAT : Application 클래스에서 Planner 객체 run 실행 처리

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,11 @@
 package christmas;
 
+import christmas.presentation.ChristmasPlanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ChristmasPlanner christmasPlanner = ChristmasPlanner.createPlanner();
+        christmasPlanner.run();
     }
 }
