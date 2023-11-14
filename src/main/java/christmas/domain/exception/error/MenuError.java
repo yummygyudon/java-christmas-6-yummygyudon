7e98208ba2086b4fbf237b41eda0f578c504873f
yummygyudon <bang2brew@gmail.com>
2023-11-15 04:08:55 +0900

    DOCS : 에러 관련 명세 주석 추가

--- src/main/java/christmas/domain/exception/error/MenuError.java
+++ src/main/java/christmas/domain/exception/error/MenuError.java
@@ -4,6 +4,7 @@ import christmas.global.exception.base.ChristmasPlannerError;
 
 public enum MenuError implements ChristmasPlannerError {
 
+    // 탐색하는 이름의 메뉴가 존재하지 않을 경우
     MENU_NOT_FOUND("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     ;
 
